c9edd702df2ed4a56d84d92c54101bdfdf0e6161
Ivan Clint A. Pabelona <ipabelona@gmail.com>
2016-02-17 09:52:43 +0800

    Backbone
    Signed-off-by: Ivan Clint A. Pabelona <ipabelona@gmail.com>

---
+++ wp-donation.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package WP-Donation
+ */
+/*
+Plugin Name: WP-Donation
+Description: Donation plugin designed for Midamerica Prison Ministry
+Author: FindingInnovation
+Author URI: http://findinginnovation.com/
+License: GPLv2 or later
+*/
