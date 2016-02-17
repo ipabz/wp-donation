9e70d0646fbf49da8c9986a17a57672ba70459d9
Ivan Clint A. Pabelona <ipabelona@gmail.com>
2016-02-17 10:12:13 +0800

    Backbone and settings page
    Signed-off-by: Ivan Clint A. Pabelona <ipabelona@gmail.com>

--- wp-donation.php
+++ wp-donation.php
@@ -9,3 +9,45 @@ Author: FindingInnovation
 Author URI: http://findinginnovation.com/
 License: GPLv2 or later
 */
+
+session_start();
+
+class WPDonation {
+	
+
+	protected static $instance = NULL;
+	
+	public static function get_instance() 
+	{
+		NULL === self::$instance and self::$instance = new self;
+		return self::$instance;
+	}
+	
+	public function __construct() 
+	{
+		add_action('init', array($this, 'init'));
+	}
+	
+	public function init()
+	{
+		add_action('admin_menu', array($this, 'wpdonation_settings_page'));
+	}
+	
+
+	function wpdonation_settings_page() {
+		add_options_page( 'WP-Donation Options', 'WP-Donation', 'manage_options', 'WP-Donation', array($this, 'wpdonation_settings_page_content') );
+	}
+
+	function wpdonation_settings_page_content() {
+		if ( !current_user_can( 'manage_options' ) )  {
+			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+		}
+		$blog_url = rtrim(site_url(), "/") . "/";
+		include 'wp-donation-settings.php';
+	}
+	
+	
+}
+
+
+WPDonation::get_instance();
