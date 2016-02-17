45ef3d23efc6b2c22238cd4d13d2ec84e979f8e7
Ivan Clint A. Pabelona <ipabelona@gmail.com>
2016-02-17 14:37:46 +0800

    updates
    Signed-off-by: Ivan Clint A. Pabelona <ipabelona@gmail.com>

--- wp-donation-settings.php
+++ wp-donation-settings.php
@@ -1,1 +1,15 @@
-contents here
+<div class="wrap">
+	<h2>WP Donation Settings</h2>
+    
+
+    <form method="post" action="options.php">
+    	<?php settings_fields('wpdonation_settings'); ?>
+		<?php do_settings_sections('wpdonation_settings'); ?> 
+
+			
+    
+		<?php submit_button(); ?>
+    </form>
+
+
+</div>
